0caf01d597e2162b331a5032c806847f1545cc93
RYAN ALVES HANADA <ryan.ahanada@senacsp.edu.br>
2025-08-14 16:26:35 -0300

    pagina pomodoro

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 //import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:herbie/pages/home_page.dart';
+import 'package:menu_negacao/pages/pomodoro_page.dart';
 
 class Herbie extends StatelessWidget {
   const Herbie({super.key});
@@ -13,7 +13,7 @@ class Herbie extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.pinkAccent),
       ),
-    home: HomePage(),
+      home: PomodoroPage(),
     );
   }
 }
