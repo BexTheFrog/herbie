c130241c02bd4c19cf05f8dc93cedf7dba931b1c
LUCAS VITORINO AQUINO ARCOVERDE <lucas.vaarcoverde@senacsp.edu.br>
2025-08-15 16:59:42 -0300

    atualizacao da pagina de perfil

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -12,25 +12,27 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xFFFFF9E9),
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(100),
+        preferredSize: Size.fromHeight(75),
         child: AppBar(
           backgroundColor: Color(0xFF5D576B),
           centerTitle: true,
-          title: Text('Perfil'),
+          title: Image.asset(
+            'assets/images/logo.png',
+            height: 50,
+          ),
         ),
       ),
-
       body: SingleChildScrollView(
         padding: EdgeInsets.all(16),
         child: Center(
           child: ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: 600),
+            constraints: BoxConstraints(maxWidth: 500),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 SizedBox(height: 50),
 
-                // Container principal do perfil com bordas arredondadas só na parte superior
+                // Container do perfil
                 Container(
                   margin: EdgeInsets.only(bottom: 24),
                   decoration: BoxDecoration(
@@ -54,6 +56,7 @@ class ProfilePage extends StatelessWidget {
                         alignment: Alignment.center,
                         clipBehavior: Clip.none,
                         children: [
+                          // Barra colorida topo
                           Container(
                             height: 70,
                             decoration: BoxDecoration(
@@ -64,6 +67,7 @@ class ProfilePage extends StatelessWidget {
                               ),
                             ),
                           ),
+                          // Avatar circular
                           Positioned(
                             top: -35,
                             child: Container(
@@ -73,25 +77,38 @@ class ProfilePage extends StatelessWidget {
                                 color: Colors.white,
                                 border: Border.all(
                                   color: Color(0xFFED6A5A),
-                                  width: 3,
+                                  width: 10,
                                 ),
                               ),
-                              child: CircleAvatar(
-                                radius: 35,
-                                backgroundImage: NetworkImage(
-                                  'https://img.freepik.com/vetores-gratis/ilustracao-desenhada-a-mao-dos-desenhos-animados-do-cerebro_52683-126692.jpg?semt=ais_hybrid&w=740&q=80',
+                              child: Semantics(
+                                label: 'User avatar',
+                                child: CircleAvatar(
+                                  radius: 50,
+                                  backgroundImage:
+                                      AssetImage("assets/images/avatar.png"),
+                                  backgroundColor: Colors.transparent,
                                 ),
-                                backgroundColor: Colors.transparent,
                               ),
                             ),
                           ),
+                          // Ícone de editar clicável
                           Positioned(
                             right: 12,
                             top: 20,
-                            child: Icon(
-                              Icons.edit,
-                              color: Color(0xFF5D576B),
-                              size: 26,
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(4),
+                              onTap: () {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text('Editar perfil clicado!'),
+                                  ),
+                                );
+                              },
+                              child: Icon(
+                                Icons.edit_square,
+                                color: Colors.white,
+                                size: 26,
+                              ),
                             ),
                           ),
                         ],
@@ -108,8 +125,8 @@ class ProfilePage extends StatelessWidget {
                       SizedBox(height: 8),
                       Container(
                         height: 2,
-                        width: 60,
-                        color: Color.fromARGB(255, 224, 170, 170),
+                        width: 350,
+                        color: Color.fromARGB(255, 255, 255, 255),
                       ),
                       SizedBox(height: 16),
                       Padding(
@@ -149,7 +166,6 @@ class ProfilePage extends StatelessWidget {
                   ),
                 ),
 
-                // --- MEUS MÓDULOS ---
                 Align(
                   alignment: Alignment.centerLeft,
                   child: Text(
@@ -157,87 +173,94 @@ class ProfilePage extends StatelessWidget {
                     style: TextStyle(
                       color: Color(0xFFED6A5A),
                       fontWeight: FontWeight.bold,
-                      fontSize: 30,
+                      fontSize: 20,
                     ),
                   ),
                 ),
                 SizedBox(height: 12),
 
                 Row(
-  children: [
-    Flexible(
-      flex: 2,
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              blurRadius: 8,
-              offset: Offset(0, 4),
-            ),
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: 250,
-              height: 50,
-              padding: EdgeInsets.all(8), // menos padding para reduzir espaço
-              decoration: BoxDecoration(
-                color: const Color(0xFFED6A5A),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(10),
+                  children: [
+                    Flexible(
+                      flex: 2,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              blurRadius: 8,
+                              offset: Offset(0, 4),
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 160 ,
+                              height: 60,
+                              padding: EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                color: const Color(0xFFED6A5A),
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(10),
+                                  topRight: Radius.circular(10),
+                                ),
+                              ),
+                              child: Text(
+                                'Desenvolvimento Web',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Color(0xFFF4F1BB),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 80),
+                              child: Text(
+                                'Técnico em desenvolvimento Web no SENAC',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF5D576B),
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 12),
+                    Flexible(
+                      flex: 1,
+                      child: GestureDetector(
+                        onTap: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Adicionar novo módulo clicado!'),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          width: 80,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            color: Color(0xFF9BC1BC),
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              child: Text(
-                'Desenvolvimento Web',
-                style: TextStyle(
-                  color: Color(0xFFF4F1BB),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 14, // reduzir um pouco o tamanho da fonte
-                ),
-              ),
-            ),
-            SizedBox(height: 6), // menos espaço
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: Text(
-                'Técnico em desenvolvimento Web no SENAC',
-                style: TextStyle(
-                  color: Color(0xFF5D576B),
-                  fontSize: 11, // fonte menor
-                ),
-              ),
-            ),
-            SizedBox(height: 10), // menos espaço
-          ],
-        ),
-      ),
-    ),
-    SizedBox(width: 12),
-    Flexible(
-      flex: 1,
-      child: Container(
-        width: 80,
-        height: 80,
-        decoration: BoxDecoration(
-          color: Color(0xFF9BC1BC),
-          borderRadius: BorderRadius.circular(6),
-        ),
-        child: Icon(
-          Icons.add,
-          size: 40,
-          color: Color(0xFF5D576B),
-        ),
-      ),
-    ),
-  ],
-)
-
               ],
             ),
           ),
